b73b58faac7b67f26a5f3246cb62676fe78dc050
Kyllingene <fuzzymuffin343@gmail.com>
2024-03-17 17:04:04 -0700

    Fixed some docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,5 @@
 #![cfg_attr(not(any(test, feature = "std")), no_std)]
-//! See [`Slice`] and [`SliceMut`].
-//!
-//! This is a collection of utilities for slices, similar to those found on
-//! iterators. The goal is to be as close to feature-parity with iterators as
-//! possible, while maintaining `no_std` compatibility.
-//!
-//! The core of this crate is providing non-contiguous slices. For example,
-//! `Slice::chain` allows you to join two slices together, clearly breaking
-//! continuity. This results in a very `Iterator`-like API. Here are some
-//! differences:
-//!
-//! - `Slice`s can only return references, not owned values - This disallows
-//! methods like `map` which require ownership semantics - `Slice`s are not
-//! lazy, and as such: - `Slice`s cannot perform arbitrary computation, because
-//! that would require allocation
-//!
-//! These shortcomings may be alleviated by a const generic API in the future,
-//! making these possible through statically known lengths.
+#![doc = include_str!("../README.md")]
 
 mod chain;
 mod chunks;
@@ -64,7 +47,8 @@ pub trait Slice<T>: Sized {
 
     /// Returns the exact length of the slice.
     ///
-    /// If infinite (e.g. through [`cycle`]), returns `usize::MAX`.
+    /// If infinite (e.g. through [`cycle`](Slice::cycle)), returns
+    /// `usize::MAX`.
     ///
     /// # Examples
     ///
