695ac5b2c01da3c75a1db6d61ecfe7e6ee22dfba
Kyllingene <fuzzymuffin343@gmail.com>
2024-03-20 15:33:53 -0700

    Added zip

---
+++ src/zip.rs
@@ -0,0 +1,31 @@
+use crate::{Slice, SliceOwned};
+
+#[derive(Debug, Clone, Copy)]
+pub struct Zip<S1, S2>(pub S1, pub S2);
+
+// TODO: can this be less strict?
+impl<S1, S2> Slice for Zip<S1, S2>
+where
+    S1: SliceOwned,
+    S2: SliceOwned,
+{
+    type Output = (S1::Output, S2::Output);
+
+    fn len(&self) -> usize {
+        self.0.len().min(self.1.len())
+    }
+
+    fn get_with<W: FnMut(&Self::Output) -> R, R>(&self, index: usize, f: &mut W) -> Option<R> {
+        Some(f(&(self.0.get_owned(index)?, self.1.get_owned(index)?)))
+    }
+}
+
+impl<S1, S2> SliceOwned for Zip<S1, S2>
+where
+    S1: SliceOwned,
+    S2: SliceOwned,
+{
+    fn get_owned(&self, index: usize) -> Option<Self::Output> {
+        Some((self.0.get_owned(index)?, self.1.get_owned(index)?))
+    }
+}
